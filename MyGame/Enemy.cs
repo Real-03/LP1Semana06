d480af69f28cd77ba3c6b512eacde2a843be157f
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-27 11:43:33 +0000

    Add methods GetHealth,GetShield and SetName

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,13 +4,14 @@ namespace Enemy
 {
     public class Enemy
     {
+
         private string name;
         private float health;
         private float shield;
 
         public Enemy(string name )
         {
-            this.name = name; // name o c u l t a t h i s . name
+            this.name = SetName(name); // name o c u l t a t h i s . name
             health = 100;
             shield = 0;
         }
@@ -18,6 +19,14 @@ namespace Enemy
         public string GetName ( )
         {
             return name ;
+        }
+        public float GetHealth ( )
+        {
+            return health ;
+        }
+        public float GetShield ( )
+        {
+            return shield ;
         }
 
         public void TakeDamage (float damage)
@@ -30,6 +39,11 @@ namespace Enemy
                 health -= damageStillToInflict ;
                 if(health<0) health = 0 ;
             }
+        }
+
+        public string SetName(string name)
+        {
+            return name.Substring(0, 8);
         }
 
 
