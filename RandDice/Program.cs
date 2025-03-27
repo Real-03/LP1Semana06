912346704ae1f13343e0f32aafc6e51ea4df3908
Tomás Real <tomasreal.art3d@gmail.com>
2025-03-27 10:59:25 +0000

    ADd RandDice project

---
+++ RandDice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random RandomNumber = new Random(Convert.ToInt32(args[1]));
+            int total =0;
+            for(int i= 0; i<Convert.ToInt32(args[0]); i++)
+            {
+                total+= RandomNumber.Next(1,6);
+            }
+            
+            Console.WriteLine(total);
+
+        }
+    }
+}
